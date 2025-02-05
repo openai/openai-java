1e4f06164802a026e4f44e1def83700b6b9dff07
Shawn Fang <shafang@microsoft.com>
2025-02-05 15:24:27 -0800

    add more new Azure service version and update azure samples

--- openai-java-example/src/main/java/com/openai/example/AzureApiKeyExample.java
+++ openai-java-example/src/main/java/com/openai/example/AzureApiKeyExample.java
@@ -1,29 +1,26 @@
 package com.openai.example;
 
-import com.azure.identity.AuthenticationUtil;
-import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.openai.azure.credential.AzureApiKeyCredential;
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
-import com.openai.credential.BearerTokenCredential;
 import com.openai.models.ChatCompletionCreateParams;
 import com.openai.models.ChatModel;
 
-public final class AzureEntraIdExample {
-    private AzureEntraIdExample() {}
+public final class AzureApiKeyExample {
+    private AzureApiKeyExample() {}
 
     public static void main(String[] args) {
         OpenAIOkHttpClient.Builder clientBuilder = OpenAIOkHttpClient.builder();
 
         /* Azure-specific code starts here */
-        // You can either set 'endpoint' directly in the builder.
-        // or set the env var "AZURE_OPENAI_ENDPOINT" and use fromEnv() method instead
+        // You can either set 'endpoint' or 'apiKey' directly in the builder.
+        // or set same two env vars and use fromEnv() method instead
         clientBuilder
                 .baseUrl(System.getenv("AZURE_OPENAI_ENDPOINT"))
-                // Set the Azure Entra ID
-                .credential(BearerTokenCredential.create(AuthenticationUtil.getBearerTokenSupplier(
-                        new DefaultAzureCredentialBuilder().build(), "https://cognitiveservices.azure.com/.default")));
+                .credential(AzureApiKeyCredential.create(System.getenv("AZURE_OPENAI_KEY")));
         /* Azure-specific code ends here */
 
+        // All code from this line down is general-purpose OpenAI code
         OpenAIClient client = clientBuilder.build();
 
         ChatCompletionCreateParams createParams = ChatCompletionCreateParams.builder()
