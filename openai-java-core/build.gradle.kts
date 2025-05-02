e3f5487df46523c31e7820f7e7c171b12d2cb7df
D Gardner <damien@gardnerhrm.com>
2025-05-02 15:24:11 +0100

    structured-outputs: updates and more unit tests.

--- openai-java-core/build.gradle.kts
+++ openai-java-core/build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
     implementation("org.apache.httpcomponents.core5:httpcore5:5.2.4")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
+    implementation("com.github.victools:jsonschema-generator:4.38.0")
+    implementation("com.github.victools:jsonschema-module-jackson:4.38.0")
 
     testImplementation(kotlin("test"))
     testImplementation(project(":openai-java-client-okhttp"))
---
+++ openai-java-core/src/main/kotlin/com/openai/core/StructuredOutputs.kt
@@ -0,0 +1,67 @@
+package com.openai.core
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.kotlinModule
+import com.github.victools.jsonschema.generator.Option
+import com.github.victools.jsonschema.generator.OptionPreset
+import com.github.victools.jsonschema.generator.SchemaGenerator
+import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder
+import com.github.victools.jsonschema.module.jackson.JacksonModule
+import com.openai.errors.OpenAIInvalidDataException
+import com.openai.models.ResponseFormatJsonSchema
+
+// The SDK `ObjectMappers.jsonMapper()` requires that all fields of classes be marked with
+// `@JsonProperty`, which is not desirable in this context, as it impedes usability. Therefore, a
+// custom JSON mapper configuration is required.
+private val MAPPER =
+    JsonMapper.builder()
+        .addModule(kotlinModule())
+        .addModule(Jdk8Module())
+        .addModule(JavaTimeModule())
+        .build()
+
+fun <T> fromClass(type: Class<T>) =
+    ResponseFormatJsonSchema.builder()
+        .jsonSchema(
+            ResponseFormatJsonSchema.JsonSchema.builder()
+                .name("json-schema-from-${type.simpleName}")
+                .schema(JsonValue.from(extractSchema(type)))
+                .build()
+        )
+        .build()
+
+internal fun <T> extractSchema(type: Class<T>): JsonNode {
+    val configBuilder =
+        SchemaGeneratorConfigBuilder(
+                com.github.victools.jsonschema.generator.SchemaVersion.DRAFT_2020_12,
+                OptionPreset.PLAIN_JSON,
+            )
+            // Add `"additionalProperties" : false` to all object schemas (see OpenAI).
+            .with(Option.FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT)
+            // Use `JacksonModule` to support the use of Jackson annotations to set property and
+            // class names and descriptions and to mark fields with `@JsonIgnore`.
+            .with(JacksonModule())
+
+    configBuilder
+        .forFields()
+        // For OpenAI schemas, _all_ properties _must_ be required. Override the interpretation of
+        // the Jackson `required` parameter to the `@JsonProperty` annotation: it will always be
+        // assumed to be `true`, even if explicitly `false` and even if there is no `@JsonProperty`
+        // annotation present.
+        .withRequiredCheck { true }
+
+    return SchemaGenerator(configBuilder.build()).generateSchema(type)
+}
+
+fun <T> fromJson(json: String, type: Class<T>): T =
+    try {
+        MAPPER.readValue(json, type)
+    } catch (e: Exception) {
+        // The JSON document is included in the exception message to aid diagnosis of the problem.
+        // It is the responsibility of the SDK user to ensure that exceptions that may contain
+        // sensitive data are not exposed in logs.
+        throw OpenAIInvalidDataException("Error parsing JSON: $json", e)
+    }
