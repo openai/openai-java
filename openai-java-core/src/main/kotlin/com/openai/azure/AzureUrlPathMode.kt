269d07f69a6021a432c16dc95d9645c92771dc64
Jose Alvarez <jp.alvarezl@gmail.com>
2025-08-19 07:39:49 +0200

    SpotlessApply

--- openai-java-core/src/main/kotlin/com/openai/azure/AzureUrlPathMode.kt
+++ openai-java-core/src/main/kotlin/com/openai/azure/AzureUrlPathMode.kt
@@ -1,10 +1,11 @@
 package com.openai.azure
 
 /**
- * Configures how the baseURL of [com.openai.core.ClientOptions] is treated for Azure OpenAI endpoints.
+ * Configures how the baseURL of [com.openai.core.ClientOptions] is treated for Azure OpenAI
+ * endpoints.
  */
 enum class AzureUrlPathMode {
-    /** Forces the deployment or model name into the path.*/
+    /** Forces the deployment or model name into the path. */
     LEGACY,
     /** [AzureOpenAIServiceVersion] is optional and the model is part of the request. */
     UNIFIED,
